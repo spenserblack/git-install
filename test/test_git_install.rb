db9c404b9e1b6f03640d708022a3960d7bde1d4b
Spenser Black <spenserblack01@gmail.com>
2021-11-12 11:03:49 -0500

    Call method with `.`

--- test/test_git_install.rb
+++ test/test_git_install.rb
@@ -7,14 +7,14 @@ class GitInstallTest < Minitest::Test
   def test_path_no_override
     old_env = ENV[INSTALL_PATH]
     ENV[INSTALL_PATH] = nil
-    assert_equal Git::Install::path, "/usr/local/bin/"
+    assert_equal Git::Install.path, "/usr/local/bin/"
     ENV[INSTALL_PATH] = old_env
   end
 
   def test_path_override
     old_env = ENV[INSTALL_PATH]
     ENV[INSTALL_PATH] = "~/bin/"
-    assert_equal Git::Install::path, "~/bin/"
+    assert_equal Git::Install.path, "~/bin/"
     ENV[INSTALL_PATH] = old_env
   end
 end
