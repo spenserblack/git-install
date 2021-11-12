ec008b12bc1409ee81e85e4c89acde2d4405000c
Spenser Black <spenserblack01@gmail.com>
2021-11-12 11:54:53 -0500

    Switch to spec style testing

--- test/test_git_install.rb
+++ test/test_git_install.rb
@@ -1,20 +1,23 @@
 require 'git/install'
 require 'minitest/autorun'
+require 'minitest/spec'
 
 INSTALL_PATH = "GIT_INSTALL_PATH"
 
-class GitInstallTest < Minitest::Test
-  def test_path_no_override
-    old_env = ENV[INSTALL_PATH]
-    ENV[INSTALL_PATH] = nil
-    assert_equal Git::Install::path, "/usr/local/bin/"
-    ENV[INSTALL_PATH] = old_env
-  end
+describe 'Git::Install' do
+  describe 'path' do
+    it 'should get the default install path' do
+      old_env = ENV[INSTALL_PATH]
+      ENV[INSTALL_PATH] = nil
+      assert_equal Git::Install.path, "/usr/local/bin/"
+      ENV[INSTALL_PATH] = old_env
+    end
 
-  def test_path_override
-    old_env = ENV[INSTALL_PATH]
-    ENV[INSTALL_PATH] = "~/bin/"
-    assert_equal Git::Install::path, "~/bin/"
-    ENV[INSTALL_PATH] = old_env
+    it 'should get the overridden path' do
+      old_env = ENV[INSTALL_PATH]
+      ENV[INSTALL_PATH] = "~/bin/"
+      assert_equal Git::Install::path, "~/bin/"
+      ENV[INSTALL_PATH] = old_env
+    end
   end
 end
